23c0009725a6b820204de8dd4339c42354536bf9
GrowTree <1547020236@qq.com>
2018-11-27 16:21:57 +0800

    init Remote1

---
+++ Example/Pods/Target Support Files/Pods-Remote1_Example/Pods-Remote1_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Remote1_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Remote1_Example
+@end
---
+++ Example/Pods/Target Support Files/Remote1/Remote1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Remote1 : NSObject
+@end
+@implementation PodsDummy_Remote1
+@end
